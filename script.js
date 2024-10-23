c7fbccc9ef07776b33ec49027c47b618bce34db9
code-push-kar <kumarpushkar681@gmail.com>
2024-10-23 23:42:19 +0530

    created the html and script file

---
+++ script.js
@@ -0,0 +1,14 @@
+let div = document.getElementById("div1");
+div.style.backgroundColor = "yellow";
+div.style.margin= "10px";
+div.style.padding = "10px";
+
+
+let para = document.getElementById("para");
+
+para.style.margin= "20px";
+para.style.padding = "10px"
+para.style.fontSize ="18px";
+para.style.fontWeight = "bold"
+para.style.height = "300px"
+para.style.width = "300px"
